418fb4bd7a0cffdf3ed642f97c882b433b3de27b
Zaid Ansari <zaid.ansari@nonstopio.com>
2023-12-12 17:06:35 +0530

    Added Full Calendar home page

--- src/App.js
+++ src/App.js
@@ -1,9 +1,19 @@
+import FullCalendar from '@fullcalendar/react';
+import dayGridPlugin from '@fullcalendar/daygrid';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">Full Calendar App</header>
+      <FullCalendar
+        headerToolbar={{
+          start: 'title', // Will show us the calendar's month & year on the start
+          end: 'today prev,next' // Will give us the option of the buttons on the right to navigate
+        }}
+        plugins={[dayGridPlugin]} // Calendar View type
+        events={[]} // List of all the events to be displayed
+      />
     </div>
   );
 }

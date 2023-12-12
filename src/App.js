7291bf73a302596c6aab0272e3facab370c240c0
Zaid Ansari <zaid.ansari@nonstopio.com>
2023-12-12 15:47:29 +0530

    Added Styling for heading

--- src/App.js
+++ src/App.js
@@ -1,8 +1,9 @@
+import './App.css';
+
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-      </header>
+      <header className="App-header">Full Calendar App</header>
     </div>
   );
 }
